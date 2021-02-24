83dab7edf1d40bc6b0dc1a4225c26f398f3beb15
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-02-24 09:43:02 -0300

    Create account

--- lib/ex_pay/users/create.ex
+++ lib/ex_pay/users/create.ex
@@ -2,11 +2,40 @@ defmodule ExPay.Users.Create do
   @moduledoc """
   Module `Create` users.
   """
-  alias ExPay.{Repo, User}
+  alias Ecto.Multi
+  alias ExPay.{Account, Repo, User}
 
   def call(attrs) do
-    attrs
-    |> User.changeset()
-    |> Repo.insert()
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(attrs))
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(repo, user)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} ->
+      preload_data(repo, user)
+    end)
+    |> run_transaction()
+  end
+
+  defp insert_account(repo, user) do
+    user.id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    params = %{user_id: user_id, balance: "0.00"}
+    Account.changeset(params)
+  end
+
+  defp preload_data(repo, user) do
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transaction(multi) do
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
--- lib/ex_pay_web/views/users_view.ex
+++ lib/ex_pay_web/views/users_view.ex
@@ -1,16 +1,28 @@
 defmodule ExPayWeb.UsersView do
   use ExPayWeb, :view
 
-  alias ExPay.User
+  alias ExPay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname, email: email}}) do
+  def render("create.json", %{
+        user: %User{
+          id: id,
+          name: name,
+          nickname: nickname,
+          email: email,
+          account: %Account{} = account
+        }
+      }) do
     %{
       message: "User created",
       user: %{
         id: id,
         name: name,
         nickname: nickname,
-        email: email
+        email: email,
+        account: %{
+          balance: account.balance,
+          id: account.id
+        }
       }
     }
   end
